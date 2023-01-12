bb875a75e48385ff48505091ac8adb39f8aa00e8
MontanoDiego <diego.e2.montano@gmail.com>
2023-01-12 13:55:48 -0800

    catchphrase list now no longer displays index number before entry

--- src/components/stats/stats.js
+++ src/components/stats/stats.js
@@ -8,8 +8,8 @@ export default function Stats({ weaponCount, armorCount, allyCount, catchphrase 
       <p className='stat-list'>You have changed the weapon {weaponCount} many times, <br></br>the armor {armorCount} many times,<br></br> and the ally {allyCount} many times!</p>
       <h4>Catchphrases!</h4>
       <ul className='list-container'>
-        {Object.entries(catchphrase).map((item, index) => (
-          <li key={index}>{item}</li>
+        {Object.entries(catchphrase).map(([key, value]) => (
+          <li key={key}>{value}</li>
         ))}
       </ul>
     </>
