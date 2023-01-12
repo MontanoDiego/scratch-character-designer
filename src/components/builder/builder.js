f210351121f51f499b1cf55c397500972e65c4e4
MontanoDiego <diego.e2.montano@gmail.com>
2023-01-12 00:40:48 -0800

    added ally options selector and preview

--- src/components/builder/builder.js
+++ src/components/builder/builder.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './builder.css';
 
 
-export default function Builder({ setWeapon, weapon, setArmor, armor }) {
+export default function Builder({ setWeapon, weapon, setArmor, armor, ally, setAlly }) {
 
   // weapons selector options list (option name, then value)
   const weapons = ['Gargoyles Great Axe', 'Great Sword', 'Winged Scythe'];
@@ -10,6 +10,9 @@ export default function Builder({ setWeapon, weapon, setArmor, armor }) {
   // armor selector options list (option name, then value)
   const armors = ['Night\'s Calvary Armor', 'Albinauric Armor', 'Blaidd\'s Armor'];
   const armorsValue = ['night', 'albi', 'blaidd'];
+  // ally selector options list (option name, then value)
+  const allies = ['Mimic Tear', 'Ancestral Follower', 'Spirit Jellyfish'];
+  const alliesValue = ['mimic', 'ancestor', 'jelly'];
   
   const handleWeaponSelect = (e) => {
     let targetValue = e.target.value;
@@ -19,25 +22,38 @@ export default function Builder({ setWeapon, weapon, setArmor, armor }) {
     let targetValue = e.target.value;
     setArmor(targetValue);
   };
+  const handleAllySelect = (e) => {
+    let targetValue = e.target.value;
+    setAlly(targetValue);
+  };
   
 
   return (
     <>
       <h2>Builder!</h2>
+      <h3>weapon!</h3>
       <select value={weapon} onChange={handleWeaponSelect}>
         {weaponsValue.map((value, index) => (
           <option key={index} value={value} >
             {weapons[index]}</option>
         ))}
       </select>
-
+      
+      <h3>armor!</h3>
       <select value={armor} onChange={handleArmorSelect}>
         {armorsValue.map((value, index) => (
           <option key={index} value={value} >
             {armors[index]}</option>
         ))}
       </select>
-
+      
+      <h3>ally!</h3>
+      <select value={ally} onChange={handleAllySelect}>
+        {alliesValue.map((value, index) => (
+          <option key={index} value={value} >
+            {allies[index]}</option>
+        ))}
+      </select>
     </>
   );
 }
--- src/components/main/main.js
+++ src/components/main/main.js
@@ -6,13 +6,15 @@ import './main.css';
 export default function Main() {
   const [weapon, setWeapon] = useState('Weapon');
   const [armor, setArmor] = useState('Armor');
+  const [ally, setAlly] = useState('Ally');
+
   return (
     <div className='main'>
       <section className='left'>
-        <Builder setWeapon={setWeapon} setArmor={setArmor}/>
+        <Builder setWeapon={setWeapon} setArmor={setArmor} setAlly={setAlly} />
       </section>
       <section className='right'>
-        <Preview weapon={weapon} armor={armor}/>
+        <Preview weapon={weapon} armor={armor} ally={ally} />
       </section>
     </div>
   );
